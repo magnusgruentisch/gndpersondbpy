78197f40a7bcb79e866fed95d019ae903dd1dd4e
magnus <magnus.gruentisch@gmail.com>
2014-02-16 20:59:54 +0100

    fix zu #1: sqlite zu integer, checkbox korrekt definiert

--- schema.sql
+++ schema.sql
@@ -1,2 +1,2 @@
 drop table if exists archiveentry;
-create table archiveentry( gnd varchar(10), vorname varchar(50),nachname varchar(50),url varchar(200),page varchar(20),comment varchar(250),ok boolean);
+create table archiveentry( gnd varchar(10), vorname varchar(50),nachname varchar(50),url varchar(200),page varchar(20),comment varchar(250),ok integer);
